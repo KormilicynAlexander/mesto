12ebaa7a640447118fdc28d0a2fff778bd47e15b
KormilicynAlexander <sashakormilicyn@mail.ru>
2023-04-24 14:46:39 +0300

    fix: ошибки выявленные на ревью

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
-let popup = document.querySelector('.pop-up');
-let popupForm = document.querySelector('.pop-up-form')
+let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup-form')
 let editButton = document.querySelector('.profile__edit-button');
-let exitButton = document.querySelector('.pop-up__exit-button');
+let exitButton = document.querySelector('.popup__exit-button');
 let profileName = document.querySelector('.profile__name');
 let profileCareer = document.querySelector('.profile__career');
-let profilePopupValues = document.querySelectorAll('.pop-up-form__input');
 let profilePopupValueName = popupForm.querySelector('#username');
 let profilePopupValueCareer = popupForm.querySelector('#career');
-let saveButton = document.querySelector('.pop-up-form__submit');
-let inputs = document.querySelectorAll('input');
 
 function openPopup() {
-    popup.classList.add('pop-up_active');
+    popup.classList.add('popup_popup_opened');
+    oldName();
 }
 
 function closePopup() {
-    popup.classList.remove('pop-up_active');
+    popup.classList.remove('popup_popup_opened');
 }
 
 function oldName () {
@@ -30,13 +28,13 @@ function handleFormSubmit (event) {
     closePopup();
 }
 
-const closePopupByClickOnOverlay = function (event) {
-    if (event.target !== event.currentTarget) return;
-    closePopup();
-}
+// const closePopupByClickOnOverlay = function (event) {
+//     if (event.target !== event.currentTarget) return;
+//     closePopup();
+// }
+
+// popup.addEventListener('click', closePopupByClickOnOverlay)
 
 editButton.addEventListener('click', openPopup);
-editButton.addEventListener('click', oldName);
 exitButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
-popup.addEventListener('click', closePopupByClickOnOverlay)
