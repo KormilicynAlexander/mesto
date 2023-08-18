14d8fb49dad3d7e08807af51a6f9eabfa9302d86
KormilicynAlexander <sashakormilicyn@mail.ru>
2023-08-18 13:09:59 +0300

    update

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ let profileName = document.querySelector('.profile__name');
 let profileCareer = document.querySelector('.profile__career');
 let profilePopupValueName = popupForm.querySelector('#username');
 let profilePopupValueCareer = popupForm.querySelector('#career');
+let elementGroupLike = document.querySelector('.element__group-like');
+
+elementGroupLike.addEventListener('click', function() {
+  elementGroupLike.classList.toggle('element__group-like_active')
+});
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -34,6 +39,45 @@ function handleFormSubmit (event) {
 // }
 
 // popup.addEventListener('click', closePopupByClickOnOverlay)
+
+const sectionElements = document.querySelector('.elements');
+
+const templateElement = document.querySelector('.template-element').content;
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  const clonedTemplateElement = templateElement.querySelector('.element').cloneNode(true);
+
+  initialCards.forEach(function(el) {
+    clonedTemplateElement.querySelector('.element__group-name').textContent = el.name;
+    clonedTemplateElement.querySelector('.element__mask-group').src = el.link;
+    sectionElements.append(clonedTemplateElement);
+  });
 
 editButton.addEventListener('click', openPopup);
 exitButton.addEventListener('click', closePopup);
